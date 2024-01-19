3f4e6e2453792b7a7da6f6b4a085abfb89e730d1
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-01-19 09:14:26 -0800

    Adding Files - 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,54 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array in ascending order using selection sort
+ * @array: The array list of integers to be sorted
+ * @size: The size of the array
+ *
+ * Return: void
+ *
+ * Description: Sorts an array using selection sort algorithm.
+ *		It takes parameters, the array list and
+ *		the array size.
+ **/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (min != i)
+		{
+			swap(&array[i], &array[min]);
+			print_array(array, sizee;
+		}
+	}
+}
+
+
+/**
+ * swap - Swaps array order in ascending order
+ * @a: Integer number
+ * @b: Integer number
+ *
+ * Return: void
+ **/
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
